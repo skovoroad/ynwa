81f31b4b41c6f94171ce457aa1de963fcc5965b6
Anton Lipkin <anton.lipkin@myoffice.team>
2026-02-06 10:50:13 +0500

    core lua functions

--- ynwa-scripts/preambles/core.lua
+++ ynwa-scripts/preambles/core.lua
@@ -15,3 +15,61 @@ end
 function my_number()
     return context.me.number
 end
+
+-- Returns my position as a table {x, y, z}
+function my_position()
+    return context.me.position
+end
+
+-- Returns ball position as a table {x, y, z}
+function ball_position()
+    return context.ball.position
+end
+
+-- Returns array of teammates (each has: index, number, position)
+function get_teammates()
+    return context.teammates
+end
+
+-- Returns array of opponents (each has: index, number, position)
+function get_opponents()
+    return context.opponents
+end
+
+-- Returns my global player index
+function my_index()
+    return context.me.index
+end
+
+-- Factory: create "kick" decision
+function kick_to(x, z, y)
+    return {
+        action = "kick",
+        target = {x = x, z = z, y = y or 0}
+    }
+end
+
+-- Factory: create "run to point" decision
+function run_to_point(x, z, y)
+    return {
+        action = "run",
+        target_type = "point",
+        target = {x = x, z = z, y = y or 0}
+    }
+end
+
+-- Factory: create "stop" decision
+function stop()
+    return {action = "stop"}
+end
+
+-- Factory: create "run to random position" decision
+function run_to_random_position()
+    local target_x = math.random() * FIELD_LENGTH
+    local target_z = math.random() * FIELD_WIDTH
+    return {
+        action = "run",
+        target_type = "point",
+        target = {x = target_x, z = target_z, y = 0}
+    }
+end
